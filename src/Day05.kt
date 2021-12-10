bdc482fc9e788db4b3c87a9ab5c18ebf3ed7a395
alexmaryin <java73@yandex.ru>
2021-12-10 12:34:35 +0400

    day 5 solved.

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,8 +1,6 @@
 fun main() {
-    fun part1(lines: List<Line>): Int {
-
-
-        return 5
+    fun part1(size: Int, lines: List<Line>): Int {
+        return processLines(size, lines)
     }
 
     fun part2(input: List<String>): Int {
@@ -10,16 +8,25 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day05_test")
-    val lines = parseInput(testInput) {
-        it.start.x == it.end.x || it.start.y == it.end.y
+    runMeasuredTime {
+        val testInput = readInput("Day05_test")
+        val testLines = parseInput(testInput)
+        check(part1(10, testLines) == 12)
     }
-    lines.sortedBy { it.direction }.forEach { println("${it.start} ${it.end}") }
-    check(part1(lines) == 5)
 
-//    val input = readInput("Day01")
-//    println(part1(input))
-//    println(part2(input))
+    runMeasuredTime {
+        println("Only horizontal and vertical lines")
+        val input = readInput("Day05")
+        val lines = parseInput(input) { it.start.x == it.end.x || it.start.y == it.end.y }
+        println(part1(1000, lines))
+    }
+
+    runMeasuredTime {
+        println("All lines including diagonals")
+        val input = readInput("Day05")
+        val lines = parseInput(input)
+        println(part1(1000, lines))
+    }
 }
 
 data class Point(val x: Int, val y: Int) : Comparable<Point> {
@@ -32,11 +39,57 @@ data class Point(val x: Int, val y: Int) : Comparable<Point> {
 
 data class Line(val start: Point, val end: Point, val direction: LineDirection)
 
-enum class LineDirection { HORIZONTAL, VERTICAL }
+enum class LineDirection { HORIZONTAL, VERTICAL, LEFT_DIAGONAL, RIGHT_DIAGONAL }
 
 fun parseInput(text: List<String>, filter: (Line) -> Boolean = { true }): List<Line> = text.map { line ->
     val (start, end) = line.split(",", " -> ", limit = 4)
         .windowed(2, step = 2)
         .map { (x, y) -> Point(x.toInt(), y.toInt()) }.sorted()
-    Line(start, end, if(start.x == end.x) LineDirection.VERTICAL else LineDirection.HORIZONTAL)
+    Line(
+        start = start,
+        end = end,
+        direction = when {
+            start.x == end.x -> LineDirection.VERTICAL
+            start.y == end.y -> LineDirection.HORIZONTAL
+            start.x < end.x && start.y < end.y -> LineDirection.LEFT_DIAGONAL
+            else -> LineDirection.RIGHT_DIAGONAL
+        }
+    )
+
 }.filter(filter)
+
+fun processLines(size: Int, lines: List<Line>): Int {
+    val field = List(size) { MutableList(size) { 0 } }
+    lines.forEach { line ->
+        when (line.direction) {
+            LineDirection.HORIZONTAL -> {
+                for (x in line.start.x..line.end.x) {
+                    field[line.start.y][x] += 1
+                }
+            }
+            LineDirection.VERTICAL -> {
+                for (y in line.start.y..line.end.y) {
+                    field[y][line.start.x] += 1
+                }
+            }
+            LineDirection.LEFT_DIAGONAL -> {
+                var y = line.start.y
+                for (x in line.start.x..line.end.x) {
+                    field[y][x] += 1
+                    y++
+                }
+            }
+            LineDirection.RIGHT_DIAGONAL -> {
+                var y = line.start.y
+                for (x in line.start.x..line.end.x) {
+                    field[y][x] += 1
+                    y--
+                }
+            }
+        }
+    }
+//    field.forEach {
+//        println(it.joinToString("") { num -> if(num > 0) num.toString() else "." })
+//    }
+    return field.sumOf { row -> row.count { it >= 2 } }
+}
